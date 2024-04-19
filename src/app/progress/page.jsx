01dac58fd361a85c83d753e45009bf87eac8ced5
Nie Shiyi <shiyi.nie@qitantech.com>
2024-04-19 17:46:04 +0800

    fix: readme

---
+++ src/app/progress/page.jsx
@@ -0,0 +1,107 @@
+import "./index.css";
+
+const steps = [
+  {
+    key: 1,
+    title: "检测开始",
+    status: "done",
+  },
+  {
+    key: 2,
+    title: "步骤1",
+    status: "done",
+  },
+  {
+    key: 3,
+    title: "步骤2",
+    status: "running",
+  },
+  {
+    key: 4,
+    title: "步骤3",
+    status: "wait",
+  },
+  {
+    key: 5,
+    title: "步骤4",
+    status: "wait",
+  },
+  {
+    key: 6,
+    title: "步骤5",
+    status: "wait",
+  },
+  {
+    key: 7,
+    title: "检测结束",
+    status: "wait",
+  },
+];
+
+// 矩形进度条
+const ProgressSquare = () => {
+  return (
+    <div className="progress">
+      <ul className="steps">
+        {steps.map((item) => {
+          const { key, title, status } = item;
+          return (
+            <li key={key} className={`step ${status}`}>
+              <i className="icon"></i>
+              <span className="text">{title}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+// 矩形进度条带描边
+const ProgressSquareWithBorder = () => {
+  return (
+    <div className="progress">
+      <ul className="steps">
+        {steps.map((item) => {
+          const { key, title, status } = item;
+          return (
+            <li key={key} className={`step ${status}`}>
+              <i className="icon"></i>
+              <div className="shape">
+                <i className="left"></i>
+                <span className="text">{title}</span>
+                <i className="right"></i>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+// 进度条集合
+const ProgressList = () => {
+  const list = [
+    {
+      title: "矩形进度条",
+      component: ProgressSquare,
+    },
+    {
+      title: "矩形进度条带描边",
+      component: ProgressSquareWithBorder,
+    },
+  ];
+
+  return (
+    <ul className="progressList">
+      {list.map((item) => (
+        <li>
+          <item.component />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ProgressList;
